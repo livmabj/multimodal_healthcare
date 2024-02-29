78d72d4a05b4bcae5eb4ba1563a51c812bd5e0e5
OGPauli <oskar@pauli.nu>
2024-02-29 09:33:06 +0000

    test example with gpu

---
+++ gemma_test.py
@@ -0,0 +1,10 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
+model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", device_map="auto")
+
+input_text = "Write me a poem about Machine Learning."
+input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+
+outputs = model.generate(**input_ids, max_length=40)
+print(tokenizer.decode(outputs[0]))
