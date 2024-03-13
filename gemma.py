7b6e02e2237d457e8e1a37c248b432aa980704a4
liv <liv.m.bjorkdahl@gmail.com>
2024-03-13 13:43:23 +0000

    preparing training

--- gemma.py
+++ gemma.py
@@ -1,38 +1,47 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+# ALL THE NECESSARY IMPORTS
+
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, HfArgumentParser, TrainingArguments
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 from tqdm import tqdm
+import pickle
+from utils import *
+
+from dataclasses import dataclass, field
+from typing import Optional
+from sklearn.model_selection import train_test_split
+
+from functools import partial
+from peft import LoraConfig, TaskType, get_peft_model, get_peft_config
 
 # Filepath to embeddings
 fname = "/mnt/mimic/data/HAIM/mimic_extras/embeddings.csv"
 
-# Loading model
+# LoRA parameter efficient fine-tuning
+# Parameters are freezed and small submodules with low-rank matrices ar inserted at the target layers.
+# initialization of model
 quantization_config = BitsAndBytesConfig(load_in_4bit=True)
-
 tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
-model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", device_map="auto", quantization_config=quantization_config)
+tokenizer.pad_token_id = tokenizer.eos_token_id
+gemma = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", device_map="auto", quantization_config=quantization_config,attn_implementation="sdpa")
+lora_config = LoraConfig(
+    r=8,
+    target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"],
+    bias="none",
+    task_type="CAUSAL_LM",
+    lora_alpha=16,
+    lora_dropout=0.1
+)
 
-# Model
-class MLPModel(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(MLPModel, self).__init__()
-        self.fc1 = nn.Linear(input_size, 512)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, output_size)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        x = self.relu(x)
-        x = self.fc3(x)
-        return x
+gemma = get_peft_model(gemma, lora_config)
 
 
-# Get and preprocess data
+# Get the data
+
 df = pd.read_csv(fname)
 df_death_small48 = df[((df['img_length_of_stay'] < 48) & (df['death_status'] == 1))]
 df_alive_big48 = df[((df['img_length_of_stay'] >= 48) & (df['death_status'] == 0))]
@@ -47,30 +56,41 @@ vd_cols = df.filter(regex='^vd_')
 y_col = df[['y']]
 haim_col = df[['haim_id']]
 df = pd.concat([haim_col, vd_cols, y_col], axis=1)
-print(df.head())
 
-input_embeddings = torch.tensor(df.iloc[:, 1:1025].values, dtype=torch.float32)
-
-
-# Transform embeddings into embeddingspace of gemma
-projection_model = MLPModel(input_size=1024, output_size=250).cuda()
-
-result_embeddings = []
-
-for emb in tqdm(input_embeddings, desc="Processing embeddings", unit="embeddings"):
-    emb = emb.cuda()
-    output_tokens = projection_model(emb)
-    normalized_output = torch.sigmoid(output_tokens)
-    scaled_output = (normalized_output * 350) + 255649
-
-    rounded_output = torch.round(scaled_output).to(torch.int)
-    result_embeddings.append(rounded_output)
+pkl_list = df['haim_id'].unique().tolist()
 
 
-# Done
-transformed_embeddings = torch.stack(result_embeddings, dim=0)
+x_train, x_val, x_test, y_train, y_val, y_test, = data_split(df, pkl_list)
 
-print(transformed_embeddings)
+# Create dataset and dataloader
+bsz = 8
 
-print(tokenizer.decode(transformed_embeddings[0]))
-print(tokenizer.decode(transformed_embeddings[0][0]))
+Trainset = CustomDataset(x_train.tolist(), y_train)
+Valset = CustomDataset(x_val, y_val)
+Testset = CustomDataset(x_test, y_test)
+
+Train_loader = DataLoader(Trainset, batch_size=bsz, collate_fn=collate_batch)
+Val_loader = DataLoader(Valset, batch_size=bsz, collate_fn=collate_batch)
+Test_loader = DataLoader(Testset, batch_size=bsz, collate_fn=collate_batch)
+
+# Run training
+
+model = ProjectionNN()
+optimizer = torch.optim.Adam(gemma.parameters(), lr=0.001)
+loss_fn = torch.nn.CrossEntropyLoss()
+num_epochs = 5
+fine_tuned, train_losses, train_accs, val_losses, val_accs = training_loop(model, gemma, optimizer, loss_fn, Train_loader, Val_loader, num_epochs)
+
+torch.save(fine_tuned, 'finetuned.pth')
+
+with open('train_losses.pkl', 'wb') as f1:
+    pickle.dump(train_losses, f1)
+
+with open('train_accs.pkl', 'wb') as f2:
+    pickle.dump(train_accs, f2)
+
+with open('val_losses.pkl', 'wb') as f3:
+    pickle.dump(val_losses, f3)
+
+with open('val_accs.pkl', 'wb') as f4:
+    pickle.dump(val_accs, f4)
