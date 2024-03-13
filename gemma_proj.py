e34b5a5c3543a9fda4cc7274872f2b6b98cf10c0
OGPauli <oskar@pauli.nu>
2024-03-13 09:40:07 +0000

    :memo: added explanation to code for readability

--- gemma_proj.py
+++ gemma_proj.py
@@ -25,7 +25,7 @@ quantization_config = BitsAndBytesConfig(load_in_4bit=True,
 tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b")
 gemma = AutoModelForCausalLM.from_pretrained("google/gemma-2b", device_map="auto", quantization_config=quantization_config)
 
-
+# Read data & extract labels and features
 df = pd.read_csv(fname)
 
 condition_death_small48 = (df['img_length_of_stay'] < 48) & (df['death_status'] == 1)
@@ -49,11 +49,13 @@ df = pd.concat([haim_col, vd_cols, y_col], axis=1)
 
 pkl_list = df['haim_id'].unique().tolist()
 
-
+# Initial prompt
 input_text = "Given this input, is it more likely than not that the patient will die?"
 input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
 word_embs = gemma.get_input_embeddings().weight[input_ids.input_ids].to("cuda")
 
+
+# Load train/val sets and create data loaders
 batch_size = 8
 x_train, x_val, y_train, y_val = data_split(df, pkl_list)
 # x_train_small, x_val_small, y_train_small, y_val_small = data_split(df.iloc[:500], pkl_list)
@@ -63,18 +65,25 @@ val_set = CustomDataset(x_val, y_val)
 train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=5)
 val_loader = DataLoader(val_set, batch_size=batch_size, num_workers=5)
 
+
+# Since the classes are very imbalanced, we weigh the classes to increase performance
 w0 = 1 - sum(y_train == 0)/len(y_train)
 w1 = 1 - sum(y_train == 1)/len(y_train)
 weights = torch.tensor([w0, w1], dtype = torch.float).to("cuda")
 
+
+# Setting model and hyperparameters
 model = ProjectionNN()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 loss_fn = nn.CrossEntropyLoss(weight=weights)
 
 num_epochs = 5
 
+# Run training
 fine_tuned, train_losses, train_accs, val_losses, val_accs = training_loop(model, gemma, optimizer, loss_fn, train_loader, val_loader, num_epochs, word_embs)
 
+
+# Save model and results
 torch.save(fine_tuned, 'finetuned.pth')
 
 with open('train_losses.pkl', 'wb') as f1:
